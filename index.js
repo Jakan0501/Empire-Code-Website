1fff4365508028ec6183075dd5cb0dceda0b6603
LucasDo <cuongbao2011@gmail.com>
2025-01-23 17:21:54 +0800

    MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express"
-import mongoose from "mongoose"
+import mongoose, { connect } from "mongoose"
 import bodyParser from "body-parser"
 import cors from "cors"
 import dotenv from "dotenv"
@@ -9,23 +9,21 @@ import courseRoute from "./routes/courseRoute.js"
 import enrollmentRoute from "./routes/enrollmentRoute.js"
 import lessonRoute from "./routes/lessonRoute.js"
 import quizRoute from "./routes/quizRoute.js"
+import { connectDB } from "./config/db.js"
 
-
+dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(bodyParser.json());
-dotenv.config();
-const PORT = process.env.PORT || 5000;
-const MONGOURL = process.env.MONGO_URL;
+console.log(process.env.MONGO_URI)
 
-mongoose.connect(MONGOURL).then(() => {
-    console.log("Database connected successful.")
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`)
-    })
-}).catch((error) => console.log(error));
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log('server started at http://localhost:' + PORT)
+})
 
 
 app.use("/api/user", userRoute);
