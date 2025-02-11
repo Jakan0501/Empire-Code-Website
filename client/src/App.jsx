b60d622643cb1fdb503543679eb14b2a86b65b2e
LucasDo <cuongbao2011@gmail.com>
2025-02-11 19:44:12 +0800

    viewcourse

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,8 @@ import Quizzes from "./pages/Quizzes/Quizzes.jsx";
 
 import CreateCourse from './pages/Courses/CreateCourses.jsx';
 import UpdateCourse from './pages/Courses/EditCourses.jsx'
+import ViewCourse from './pages/Courses/ViewCourse.jsx'
+
 
 import Navbar from "./components/Navbar"; // Adjust the path as necessary
 import Sidebar from "./components/Sidebar.jsx";
@@ -65,7 +67,7 @@ function App() {
 
         <Route path="/createCourse" element={<CreateCourse />}/>
         <Route path="/courses/update/:id" element={<UpdateCourse />}/>
-
+        <Route path="/viewCourse/:id" element={<UpdateCourse />}/>
       </Routes>
     </BrowserRouter>
   );
